54cf490f476db497f4d35dcc49c8d82129f7e8bf
John Mook <jedwardmook@yahoo.com>
2022-01-03 03:21:29 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+function writeCards(names,occasion) {
+    let thankYous = []
+    for (let i = 0; i < names.length; i++) {
+        thankYous.push (`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`)
+    }
+    return thankYous;
+}
+writeCards(names,occasion);
+
+function countDown() {
+    let countDown = 10;
+    while (countDown >= 0) {
+        console.log(countDown--);
+        debugger;
+    }
+}
+countDown(10);
